800fdc8c43e8b9b9d99eb482e95a8b75ff1301c0
Estalibar <reinsagersr@yahoo.com>
2025-07-22 17:33:57 -1000

    Bookbot Complete

--- main.py
+++ main.py
@@ -9,18 +9,23 @@ def get_book_text(filepath):
     return file_contents
 
 def main():
-    book_text = get_book_text()
-    num_words = get_num_words(book_text)
-    num_char = get_distinct_char(book_text)
-    sorted_list = pretty_list(num_char)
-    print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
-    print("----------- Word Count ----------")
-    print(f"Found {num_words} total words")
-    print("--------- Character Count -------")
-    for char_count in sorted_list:
-        key, value = list(char_count.items())[0]
-        print(f"{key}: {value}")
-    print("============= END ===============")
+    if len(sys.argv) == 2:
+        book_text = get_book_text(sys.argv[1])
+        num_words = get_num_words(book_text)
+        num_char = get_distinct_char(book_text)
+        sorted_list = pretty_list(num_char)
+        book = sys.argv[1]
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {sys.argv[1]}...")
+        print("----------- Word Count ----------")
+        print(f"Found {num_words} total words")
+        print("--------- Character Count -------")
+        for char_count in sorted_list:
+            key, value = list(char_count.items())[0]
+            print(f"{key}: {value}")
+        print("============= END ===============")
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 
 main()
